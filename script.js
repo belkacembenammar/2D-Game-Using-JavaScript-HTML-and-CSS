6ca4abe0ba1545ab91b16319e2c301fa50a99592
belkacembenammar <94174782+belkacembenammar@users.noreply.github.com>
2023-09-17 16:32:51 +0200

    Basic Game canvas + Player + Laser Shoot

---
+++ script.js
@@ -0,0 +1,221 @@
+window.addEventListener('load', function() {
+    const canvas = this.document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
+
+    class InputHandler 
+    {
+        constructor(game)
+        {
+            this.game = game;
+
+            window.addEventListener('keydown', e => {
+                if ((e.key === "ArrowUp"
+                    || e.key === "ArrowDown"
+                ) && this.game.keys.indexOf(e.key) === -1)
+                {
+                    this.game.keys.push(e.key);
+                }
+                else if (e.key === ' ')
+                {
+                    this.game.player.shootTop();
+                }
+                console.log(this.game.keys);
+            });
+
+            window.addEventListener('keyup', e  => {
+                let indexOfKey = this.game.keys.indexOf(e.key);
+                if (indexOfKey > -1)
+                {
+                    this.game.keys.splice(indexOfKey, 1);
+                }
+            });
+        }
+    }
+
+    class Projectile 
+    {
+        constructor(game, x, y)
+        {
+            this.game = game;
+            this.x = x;
+            this.y = y;
+            this.width = 10;
+            this.height = 3;
+            this.speed = 3;
+            this.markedForDeletion = false;
+        }
+
+        update()
+        {
+            this.x += this.speed;
+            if (this.x > this.game.width * 0.8) this.markedForDeletion = true;
+        }
+
+        draw(context)
+        {
+            context.fillStyle = 'yellow';
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+
+    class Particle
+    {
+
+    }
+
+    class Player
+    {
+        constructor(game)
+        {
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+            this.maxSpeed = 2;
+            this.projectiles = [];
+        }
+
+        update()
+        {
+            if (this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
+            else if (this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
+            else this.speedY = 0;
+            this.y += this.speedY;
+            this.projectiles.forEach(projectile => {
+                projectile.update();
+            });
+            this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion)
+        }
+
+        draw(context)
+        {
+            context.fillStyle = 'black';
+            context.fillRect(this.x, this.y, this.width, this.height);
+            this.projectiles.forEach(projectile => {
+                projectile.draw(context);
+            });
+        }
+
+        shootTop()
+        {
+            if (this.game.ammo > 0)
+            {
+                this.projectiles.push(new Projectile(this.game, this.x, this.y));
+                this.game.ammo--;
+            }
+        }
+    }
+
+    class Enemy
+    {
+        constructor(game)
+        {
+            this.game = game;
+            this.x = this.game.width;
+            this.speedX = Math.random() * -1.5 - 0.5;
+            this.markedForDeletion = false;
+        }
+
+        update()
+        {
+            this.x += this.speedX;
+            if (this.x + this.game.width < 0) this.markedForDeletion = true;
+        }
+
+        draw(context)
+        {
+            context.fillStyle = 'red';
+            context.fillRect(this.x, this.y, this.game.width, this.game.height);
+        }
+    }
+
+    class AnglerFish extends Enemy
+    {
+        
+    }
+
+    class Layer 
+    {
+
+    }
+
+    class Background
+    {
+
+    }
+
+    class UI
+    {
+        constructor(game)
+        {
+            this.game = game;
+            this.fontSize = 25;
+            this.fontFamily = 'Helvetica';
+            this.color = 'yellow';
+        }
+
+        draw(context)
+        {
+            context.fillStyle = this.color;
+            for (let i = 0; i < this.game.ammo; i++)
+            {
+                context.fillRect(20 + 6 * i, 10, 5, 10);
+            }
+        }
+    }
+
+    class Game
+    {
+        constructor(width, height)
+        {
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+            this.ui = new UI(this);
+            this.input = new InputHandler(this);
+            this.keys = [];
+            this.ammo = 20;
+            this.maxAmmo = 50;
+            this.ammoTimer = 0;
+            this.ammoInterval = 500;
+        }
+
+        update(deltaTime)
+        {
+            this.player.update();
+            if (this.ammoTimer > this.ammoInterval)
+            {
+                if (this.ammo < this.maxAmmo) this.ammo++;
+                this.ammoTimer = 0;
+            }
+            else
+            {
+                this.ammoTimer += deltaTime;
+            }
+        }
+
+        draw(context)
+        {
+            this.player.draw(context);
+            this.ui.draw(context);
+        }
+    }
+
+    const game = new Game(canvas.width, canvas.height);
+    let lastTime = 0;
+    //animation loop
+    function animate(timeStamp)
+    {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        game.update(deltaTime);
+        game.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate(0);
+});
